f7092ee5376eeeca170a04e305f78bccf1966a33
Mahdiar Jamali <mahdyarjmofficial@gmail.com>
2026-06-21 20:59:06 +0330

    move auth routes and add middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-const express = require("express")
-const authRoutes = require("./routes/auth")
-const app = express()
+const express = require("express");
+const authRoutes = require("./routes/auth");
+const app = express();
 
-app.use("/api" , authRoutes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//auth routes:
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+  console.log("Server is running on http://localhost:3000");
+});
