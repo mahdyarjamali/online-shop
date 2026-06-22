c326a9c434c785b571cfdf1bd64a304726e66f46
Mahdiar Jamali <mahdyarjmofficial@gmail.com>
2026-06-22 19:54:42 +0330

    add discount creation endpoint

---
+++ src/routes/discounts.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("shop.db");
+
+const applyDiscount = function (req, res) {
+  const discountData = [
+    req.body.code,
+    req.body.percentage,
+    req.body.expires_at,
+  ];
+
+  db.serialize(() => {
+    db.run(
+      `insert into discounts (code , percentage , expires_at)
+            values (? , ? , ?)`,
+      discountData,
+      (err) => {
+        if (err) {
+          res.send(err.message);
+        } else {
+          res.send("Discount created successfully");
+        }
+      },
+    );
+  });
+};
+//
+//
+//
+//
+router.post("/create_discount" , applyDiscount)
+//
+//
+//
+//
+module.exports = router
