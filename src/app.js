a1432e7f65ef28ecc2548d57929975327bc05cc8
Mahdiar Jamali <mahdyarjmofficial@gmail.com>
2026-06-22 17:22:21 +0330

    add product and cart routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const authRoutes = require("./routes/auth");
+const productRoutes = require("./routes/products");
+const cartRoutes = require("./routes/carts");
+
 const app = express();
 
 app.use(express.json());
@@ -8,6 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 
 //auth routes:
 app.use("/api/auth", authRoutes);
+
+// product routes:
+app.use("/api/products", productRoutes);
+
+// cart routes:
+app.use("/api/carts", cartRoutes);
+
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
