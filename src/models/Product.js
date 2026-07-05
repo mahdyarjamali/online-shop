7d7b371e25e9a05a6ca83073dffb279a48b3373b
Mahdiar Jamali <mahdyarjmofficial@gmail.com>
2026-07-05 22:55:47 +0330

    refactor(products): move product database logic to model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,16 +1,14 @@
-const db = require("../db")
-const User = require("./User")
-const { create } = require("./User")
+const db = require("../db");
 
 const Product = {
-    create : function(productData , callback){
-        db.run(
-            `insert into products (name , description , price , category , stock , image)
+  create: function (productData, callback) {
+    db.run(
+      `insert into products (name , description , price , category , stock , image)
             values (? , ? , ? , ? , ? , ?)`,
-            productData,
-            callback
-        )
-    }
-}
+      productData,
+      callback,
+    );
+  },
+};
 
-module.exports = Product
+module.exports = Product;
